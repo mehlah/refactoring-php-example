42e9833a5806e366cf399fbe132292583d5f4204
Mehdi Lahmam B <mehdi@lahmam.com>
2012-12-27 17:40:55 +0100

    Rename some variables in amountFor method for better clarity

--- lib/Customer.php
+++ lib/Customer.php
@@ -50,27 +50,27 @@ class Customer {
 	}
 
 	private function amountFor(Rental $rental) {
-		$thisAmount = 0;
+		$result = 0;
 
 		switch ($rental->getMovie()->getPriceCode()) {
 			case Movie::REGULAR :
-				$thisAmount += 2;
+				$result += 2;
 				if ($rental->getDaysRented() > 2) {
-					$thisAmount += ($rental->getDaysRented() - 2) * 1.5;
+					$result += ($rental->getDaysRented() - 2) * 1.5;
 				}
 			break;
 			case Movie::NEW_RELEASE :
-				$thisAmount += $rental->getDaysRented() * 3;
+				$result += $rental->getDaysRented() * 3;
 			break;
 			case Movie::CHILDREN :
-				$thisAmount += 1.5;
+				$result += 1.5;
 				if ($rental->getDaysRented() > 3) {
-					$thisAmount += ($rental->getDaysRented() - 3) * 1.5;
+					$result += ($rental->getDaysRented() - 3) * 1.5;
 				}
 			break;
 		}
 
-		return $thisAmount;
+		return $result;
 	}
 }
 
