c69dd43f2b21cd22d658774b2e007b0b48e0ce6a
Mehdi Lahmam B <mehdi@lahmam.com>
2012-12-28 14:52:30 +0100

    Replace Movie's getCharge conditional with a type code behavior in the Price object

--- lib/Movie.php
+++ lib/Movie.php
@@ -40,27 +40,7 @@ class Movie {
 	}
 
 	public function getCharge($daysRented) {
-		$result = 0;
-
-		switch ($this->getPriceCode()) {
-			case Movie::REGULAR :
-				$result += 2;
-				if ($daysRented > 2) {
-					$result += ($daysRented - 2) * 1.5;
-				}
-			break;
-			case Movie::NEW_RELEASE :
-				$result += $daysRented * 3;
-			break;
-			case Movie::CHILDREN :
-				$result += 1.5;
-				if ($daysRented > 3) {
-					$result += ($daysRented - 3) * 1.5;
-				}
-			break;
-		}
-
-		return $result;
+		return $this->_price->getCharge($daysRented);
 	}
 
 	public function getFrequentRenterPoints($daysRented) {
--- lib/Price.php
+++ lib/Price.php
@@ -4,11 +4,21 @@ require_once 'Movie.php';
 
 abstract class Price {
 	abstract public function getPriceCode();
+	abstract public function getCharge($daysRented);
 }
 
 class ChildrensPrice extends Price {
 	public function getPriceCode() {
 		return Movie::CHILDREN;
+	}
+
+	public function getCharge($daysRented) {
+		$result = 1.5;
+		if ($daysRented > 3) {
+			$result += ($daysRented - 3) * 1.5;
+		}
+
+		return $result;
 	}
 }
 
@@ -16,11 +26,24 @@ class NewReleasePrice extends Price {
 	public function getPriceCode() {
 		return Movie::NEW_RELEASE;
 	}
+
+	public function getCharge($daysRented) {
+		return $daysRented * 3;
+	}
 }
 
 class RegularPrice extends Price {
 	public function getPriceCode() {
 		return Movie::REGULAR;
+	}
+
+	public function getCharge($daysRented) {
+		$result = 2;
+		if ($daysRented > 2) {
+			$result += ($daysRented - 2) * 1.5;
+		}
+
+		return $result;
 	}
 }
 
