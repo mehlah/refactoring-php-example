94a1c5916bd3f30b8980cd2fa4f49595b6da6f0e
Mehdi Lahmam B <mehdi@lahmam.com>
2012-12-28 12:58:02 +0100

    Move getCharge et getFrequentRenterPoints from Rental to Movie class

--- lib/Movie.php
+++ lib/Movie.php
@@ -24,6 +24,38 @@ class Movie {
 	public function getTitle() {
 		return $this->_title;
 	}
+
+	public function getCharge($daysRented) {
+		$result = 0;
+
+		switch ($this->getPriceCode()) {
+			case Movie::REGULAR :
+				$result += 2;
+				if ($daysRented > 2) {
+					$result += ($daysRented - 2) * 1.5;
+				}
+			break;
+			case Movie::NEW_RELEASE :
+				$result += $daysRented * 3;
+			break;
+			case Movie::CHILDREN :
+				$result += 1.5;
+				if ($daysRented > 3) {
+					$result += ($daysRented - 3) * 1.5;
+				}
+			break;
+		}
+
+		return $result;
+	}
+
+	public function getFrequentRenterPoints($daysRented) {
+		if (($this->getPriceCode() == Movie::NEW_RELEASE) && ($daysRented > 1)) {
+				return 2;
+		} else {
+			return 1;
+		}
+	}
 }
 
 ?>
